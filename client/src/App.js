6769f73098adc0b425c2461940ab8960d4489558
Trung <trungtd@perfin.io>
2023-01-08 12:44:12 +0700

    up

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
-  return (
-    <div className="App">
-      <div>Hello world</div>
-    </div>
-  );
+    return (
+        <div className="App">
+            <div>Hello world</div>
+        </div>
+    );
 }
 
 export default App;
