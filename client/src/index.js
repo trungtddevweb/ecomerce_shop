a0f66a7722496d185ae2298da02922aab960a611
Trung <trungtd@perfin.io>
2023-01-17 15:03:36 +0700

    add redux,...

--- client/src/index.js
+++ client/src/index.js
@@ -1,16 +1,22 @@
 import React, { lazy, Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.scss'
-
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistor, store } from 'src/redux/store'
+import { Provider } from 'react-redux';
 const LazyApp = lazy(() => import('./App'))
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <Suspense fallback={<div>Loading...</div>}>
-            <LazyApp />
-        </Suspense>
+        <Provider store={store} >
+            <PersistGate loading={null} persistor={persistor}>
+                <Suspense fallback={<div>Loading...</div>}>
+                    <LazyApp />
+                </Suspense>
+            </PersistGate>
+        </Provider>
     </React.StrictMode>
 
 );
