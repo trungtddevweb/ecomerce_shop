21d27b9fa806c9a41d3a256619753debb6cd3046
Trung <trungtd@perfin.io>
2023-01-29 22:59:20 +0700

    add routes page

--- client/src/pages/ErrorPage/ErrorPage.js
+++ client/src/pages/ErrorPage/ErrorPage.js
@@ -1,17 +1,27 @@
 import { useRouteError } from 'react-router-dom'
+import routes from 'src/utils/routes';
+import Button from '~/components/Button';
 
 const ErrorPage = () => {
     const error = useRouteError();
 
     return (
-        <div id='error-page'>
-            <h1>Oops!</h1>
-            <p>Sorry, an unexpected error has occurred.</p>
-            <p>
-                <i>{error.statusText || error.message}</i>
-            </p>
+        <div className="d-flex align-items-center justify-content-center vh-100">
+            <div className="text-center">
+                <h1 className="display-1 fw-bold">404</h1>
+                <p className="fs-3"> <span className="text-danger">Opps!</span> Page not found.</p>
+                <p className="lead">
+                    The page you’re looking for doesn’t exist.
+                </p>
+                <p>
+                    <i>{error.statusText || error.message}</i>
+                </p>
+                <Button to={routes.home.path} className="btn btn-primary">Go Home</Button>
+            </div>
         </div>
     )
 }
+
+
 
 export default ErrorPage
--- client/src/utils/routes.js
+++ client/src/utils/routes.js
@@ -1,7 +1,11 @@
+import AboutPage from "~/pages/AboutPage";
+import BlogPage from "~/pages/BlogPage";
+import ContactPage from "~/pages/ContactPage";
 import HomePage from "~/pages/HomePage";
 import LoginPage from "~/pages/LoginPage";
 import ProductPage from "~/pages/ProductPage";
 import RegisterPage from "~/pages/RegisterPage";
+import SearchPage from "~/pages/SearchPage";
 
 const routes =
 {
@@ -24,6 +28,22 @@ const routes =
     product: {
         path: '/products/:productId',
         element: <ProductPage />
+    },
+    blog: {
+        path: '/blogs',
+        element: <BlogPage />
+    },
+    contact: {
+        path: '/contact',
+        element: <ContactPage />
+    },
+    search: {
+        path: '/search',
+        element: <SearchPage />
+    },
+    about: {
+        path: '/about',
+        element: <AboutPage />
     }
 
 }
