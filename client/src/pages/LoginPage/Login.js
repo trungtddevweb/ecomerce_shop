0982ef0f4a8ab27395d79f75ffa991a79c74f400
tambaki123 <89337337+tambaki123@users.noreply.github.com>
2023-02-19 10:33:22 +0700

    login

--- client/src/pages/LoginPage/Login.js
+++ client/src/pages/LoginPage/Login.js
@@ -1,7 +1,64 @@
-import React from 'react';
+import React from 'react'
+import { useForm } from 'react-hook-form'
+import Button from 'react-bootstrap/Button'
+import Form from 'react-bootstrap/Form'
 
 const Login = () => {
-    return <div>Login Page</div>;
-};
+    const {
+        register,
+        handleSubmit,
+        formState: { errors }
+    } = useForm()
+    const onSubmit = data => console.log(data)
+    return (
+        <>
+            <div className='form-container d-flex justify-content-center align-items-center'>
+                <Form onSubmit={handleSubmit(onSubmit)} className='shadow rounded p-4 form-wrap'>
+                    <div className='img-wrap'>
+                        <img src='https://cdn.pixabay.com/photo/2020/05/21/11/13/shopping-5200288_1280.jpg' alt='' />
+                    </div>
+                    <h3 className='text-center mb-5'>Welcome Back!</h3>
+                    <div className='form-floating mb-3'>
+                        <input
+                            type='text'
+                            className='form-control'
+                            id='floatingInput'
+                            placeholder='Username'
+                            {...register('username', { required: true })}
+                        />
+                        <label htmlFor='floatingInput'>Username</label>
+                    </div>
+                    {errors.username?.type === 'required' && (
+                        <p className='text-danger'>Username không được để trống</p>
+                    )}
+                    <div className='form-floating  mb-3'>
+                        <input
+                            type='password'
+                            className='form-control'
+                            id='floatingInput'
+                            placeholder='Passowrd'
+                            {...register('password', { required: true })}
+                        />
+                        <label htmlFor='floatingInput'>Password</label>
+                    </div>
+                    {errors.password?.type === 'required' && (
+                        <p className='text-danger'>Password không được để trống</p>
+                    )}
 
-export default Login;
+                    <div className='d-flex justify-content-between'>
+                        <p>Forget Password?</p>
+                        <a href='/register'>
+                            <p className='text-primary'>Register now!</p>
+                        </a>
+                    </div>
+
+                    <Button type='submit' variant='primary' className='btn'>
+                        Login
+                    </Button>
+                </Form>
+            </div>
+        </>
+    )
+}
+
+export default Login
--- client/src/pages/RegisterPage/RegisterPage.js
+++ client/src/pages/RegisterPage/RegisterPage.js
@@ -1,9 +1,74 @@
 import React from 'react'
 
+import { useForm } from 'react-hook-form'
+import { yupResolver } from '@hookform/resolvers/yup'
+import * as yup from 'yup'
+import Button from 'react-bootstrap/Button'
+import Form from 'react-bootstrap/Form'
+const registerData = yup.object().shape({
+    username: yup.string().required('Username không được để trống'),
+    password: yup.string().required('Password không được để trống').min(6),
+    confirmPassowrd: yup.string().oneOf([yup.ref('password'), null])
+})
 const RegisterPage = () => {
-  return (
-    <div>RegisterPage</div>
-  )
+    const {
+        register,
+        handleSubmit,
+        formState: { errors }
+    } = useForm({ resolver: yupResolver(registerData) })
+    const onSubmit = data => console.log(data)
+    return (
+        <>
+            <div className=' form-container d-flex justify-content-center align-items-center'>
+                <Form onSubmit={handleSubmit(onSubmit)} className=' shadow p-4 rounded form-wrap'>
+                    <div className='img-wrap'>
+                        <img src='https://cdn.pixabay.com/photo/2020/05/21/11/13/shopping-5200288_1280.jpg' alt='' />
+                    </div>
+                    <h3 className='text-center mb-5'>Register Form</h3>
+                    <div className='form-floating mb-3'>
+                        <input
+                            type='text'
+                            className='form-control'
+                            id='floatingInput'
+                            placeholder='Username'
+                            {...register('username', { required: true })}
+                        />
+                        <label htmlFor='floatingInput'>Username</label>
+                    </div>
+                    {errors.username && <p className='text-danger'>{errors.username.message}</p>}
+
+                    <div className='form-floating mb-3'>
+                        <input
+                            type='password'
+                            className='form-control'
+                            id='floatingInput'
+                            placeholder='Password'
+                            {...register('password', { required: true, minLength: 6 })}
+                        />
+
+                        <label htmlFor='floatingInput'>Password</label>
+                    </div>
+                    {errors.password && <p className='text-danger'>{errors.password.message}</p>}
+                    {errors.password?.type === 'minLength' && <p>Password must be 6 characters long</p>}
+                    <div className='form-floating mb-3'>
+                        <input
+                            type='password'
+                            className='form-control'
+                            id='floatingInput'
+                            placeholder='ConfirmPassowrd'
+                            {...register('confirmPassowrd', { required: true })}
+                        />
+                        <label htmlFor='floatingInput'>ConfirmPassowrd</label>
+                    </div>
+
+                    {errors.confirmPassowrd && <p className='text-danger'>password don't match</p>}
+                    <Button type='submit' variant='primary' className='btn'>
+                        Register
+                    </Button>
+                </Form>
+            </div>
+        </>
+    )
 }
 
 export default RegisterPage
