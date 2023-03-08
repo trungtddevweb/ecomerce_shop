7e465ab84bffee15ae2ea2c48142102486d690df
Trung <trungtd@perfin.io>
2023-03-08 19:31:50 +0700

    up

--- client/src/layouts/components/Header/Header.js
+++ client/src/layouts/components/Header/Header.js
@@ -1,11 +1,43 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faCartShopping } from "@fortawesome/free-solid-svg-icons"
 import Search from "../Search"
-import routes from "src/utils/routes"
+// import routes from "src/utils/routes"
 import { Container, Nav, NavDropdown, Navbar, NavbarBrand } from "react-bootstrap"
 import { LinkContainer } from "react-router-bootstrap"
+import Button from "~/components/Button/Button"
+import { ExitToApp } from "@mui/icons-material"
+import { useDispatch } from "react-redux"
+import { useState } from "react"
+import { logoutSuccess } from "src/redux/slice/usersSlice"
+import Loading from "~/components/Loading/Loading"
+import { logout } from "~/api/main"
+import { useNavigate } from "react-router-dom"
+import { useEffect } from "react"
 
 const Header = () => {
+    const dispatch = useDispatch()
+    const [errors, setErrors] = useState(null)
+    const [isLoading, setIsLoading] = useState(false)
+    const user = JSON.parse(localStorage.getItem('user'))
+    const navigate = useNavigate()
+    useEffect(() => {
+        if (!user) return navigate('/login')
+
+    }, [user, navigate])
+
+    console.log("user----", user)
+    const handleLogout = async () => {
+        setIsLoading(true)
+        try {
+            await logout()
+            dispatch(logoutSuccess())
+            setIsLoading(false)
+        } catch (error) {
+            setIsLoading(false)
+            setErrors(error.message)
+            console.log("Error", errors)
+        }
+    }
 
     return (
         <Navbar expand="lg" sticky="top" className="header bg-light">
@@ -47,7 +79,17 @@ const Header = () => {
                     </Nav>
                     <Nav className="d-flex gap-2">
                         <Search />
-                        <div>{ }</div>
+                        {
+                            isLoading ? (
+                                <div>
+                                    <Loading />
+                                </div>
+                            ) : (
+                                <Button rightIcon={<ExitToApp onClick={handleLogout} />}>
+                                    Hello username
+                                </Button>
+                            )
+                        }
                         <Nav className="cart-shopping p-2">
                             <FontAwesomeIcon icon={faCartShopping} />
                             <div className="count-product bg-danger text-white p-1 fw-bold rounded-circle">10</div>
