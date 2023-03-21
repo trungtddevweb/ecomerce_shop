4bf912d87e8e158688ca3d724e21a5413fc479dd
tambaki123 <89337337+tambaki123@users.noreply.github.com>
2023-03-21 14:39:50 +0700

    slider

--- client/src/pages/HomePage/HomePage.js
+++ client/src/pages/HomePage/HomePage.js
@@ -1,24 +1,24 @@
-import { useEffect } from "react"
+import { useEffect } from 'react'
+import Sliders from '~/components/Slider/Sliders'
 
 const HomePage = () => {
-
-
-  // const [data, setData] = useState([])
-  useEffect(() => {
-    const fetchPost = async () => {
-      try {
-        // const res = await getAllProducts()
-      } catch (error) {
-        console.log(error)
-      }
-    }
-    fetchPost()
-  }, [])
-  return (
-    <div>
-      <div>Hello</div>
-    </div>
-  )
+    // const [data, setData] = useState([])
+    useEffect(() => {
+        const fetchPost = async () => {
+            try {
+                // const res = await getAllProducts()
+            } catch (error) {
+                console.log(error)
+            }
+        }
+        fetchPost()
+    }, [])
+    return (
+        <div>
+            <div>Hello</div>
+            <Sliders />
+        </div>
+    )
 }
 
 export default HomePage
