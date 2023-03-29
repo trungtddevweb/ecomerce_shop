d16a99d75cde7a455ee8487de111063d0b0925f2
Trung <trungtd@perfin.io>
2023-03-30 00:47:19 +0700

    up

--- client/src/pages/HomePage/HomePage.js
+++ client/src/pages/HomePage/HomePage.js
@@ -1,15 +1,15 @@
 import { useEffect, lazy } from "react"
 import { useSelector } from "react-redux"
 import useDocumentTitle from "src/hooks/useDocumentTitle"
+import { getToken } from "src/utils/const"
 
 const Sliders = lazy(() => import('~/components/Slider'))
 
 
 const HomePage = () => {
   useDocumentTitle('Trang chủ')
-  const user = useSelector(state => state.auth.user)
-  console.log(user)
-
+  // const token = useSelector(getToken)
+  // console.log(token)
 
 
   // const [data, setData] = useState([])
