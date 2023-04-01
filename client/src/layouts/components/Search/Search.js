fac36a4578dfa83f70127448dd51d905d9fc5d13
tambaki123 <89337337+tambaki123@users.noreply.github.com>
2023-04-01 15:58:25 +0700

    footer-demo

--- client/src/layouts/components/Search/Search.js
+++ client/src/layouts/components/Search/Search.js
@@ -1,28 +1,28 @@
-import { IconButton, TextField, Box } from "@mui/material";
+import { IconButton, TextField, Box } from '@mui/material'
 import { Search as SearchIcon } from '@mui/icons-material'
 
 const Search = () => {
+    const search = () => {
+        console.log('helle')
+    }
     return (
-        <Box component="form"
-            noValidate
-            autoComplete="off"
-        >
+        <Box component='form' noValidate autoComplete='off'>
             <TextField
-                id="search"
-                label="Tìm sản phẩm..."
-                variant="outlined"
-                size="small"
+                id='search'
+                label='Tìm sản phẩm...'
+                variant='outlined'
+                size='small'
                 fullWidth
                 InputProps={{
                     endAdornment: (
-                        <IconButton type="submit" aria-label="search">
+                        <IconButton aria-label='search' onClick={search}>
                             <SearchIcon />
                         </IconButton>
-                    ),
+                    )
                 }}
             />
         </Box>
-    );
+    )
 }
 
 export default Search
